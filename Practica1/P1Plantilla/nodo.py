6da30417c176764de1d5372fc851a900ae19a42a
Pablo Bernabeu <pabc1@gcloud.ua.es>
2021-10-22 12:08:45 +0200

    buff

--- Practica1/P1Plantilla/nodo.py
+++ Practica1/P1Plantilla/nodo.py
@@ -3,17 +3,24 @@ def calcularG(casillaActual, casillaAnterior):
         return 1.5
     else:
         return 1
+    
+
+def distanciaManhattan(cas1, cas2):
+    return abs(cas1.getFila() - cas2.getFila()) + abs(cas1.getCol() - cas2.getCol())
+    
 
 class Nodo():
     def __init__(self, cas, casAnterior, destino):
         self.casilla = cas
         self.g = calcularG(cas, casAnterior)
+        # self.h = distanciaManhattan(destino, cas)
         self.h = 0
         self.f = self.g + self.h
         self.padre = None
         
     def __eq__(self, nodo2):
         return (self.casilla.getFila() == nodo2.getCasilla().getFila() and self.casilla.getCol() == nodo2.getCasilla().getCol())
+
 
     def getG(self):
         return self.g
@@ -27,16 +34,31 @@ class Nodo():
     def getCasilla(self):
         return self.casilla
     
-    def getPadre(self):
-        return self.padre
-    
     def setG(self, newG):
         self.g = newG
-        self.f = self.g + self.h
+        
+    def setH(self, destino):
+        # self.h = distanciaManhattan(destino, self.casilla)
+        self.h = 0
+        
+    def setF(self, newF):
+        self.f = newF
+        
+    def setPadre(self, p):
+        self.padre = p
     
     def esMeta(self, destino):
         return (self.casilla.getFila() == destino.getFila() and self.casilla.getCol() == destino.getCol())
     
+<<<<<<< HEAD
+<<<<<<< HEAD
+    
+        
+=======
     def setPadre(self, p):
         self.padre = p
+>>>>>>> parent of cedc903 (Terminando el trabajo para la sesión 3.)
+=======
+    
         
+>>>>>>> 656bbae7c1340b9245cbd865c31adb2c56e99c6a
